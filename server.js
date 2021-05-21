6d195f68f1ac576cf4a3fdbc1f602cae1e9a5873
Paul Marceau <paul@themarceaus.net>
2021-05-20 23:53:35 -0400

    Added the code to for the routes and parsing the note text

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Required modules, packages, etc
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //Create the express server
 const app = express();
@@ -7,7 +9,16 @@ const app = express();
 //Set the listener port (use the Heroku provided or the default of 8080)
 const PORT = process.env.PORT || 8080;
 
+//linking public folder to attain data inside for this application
+app.use(express.static("public"));
 
+// Parse incoming string or array data and the JSON data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes needed to handle the user requests
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // LISTENER: Let's us know the server is running
 app.listen(PORT, () => {
